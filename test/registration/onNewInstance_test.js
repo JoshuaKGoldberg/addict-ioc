ee99e5e0e2ed25c1c8d9647dd451e49dc770fdad
5ebastianMeier <sebastian.meier@5minds.de>
2016-04-09 15:59:54 +0200

    Add unit test for onNewInstance declaration

---
+++ test/registration/onNewInstance_test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const should = require('should');
+
+const container = require('./../../lib/container.js');
+
+class TestType {}
+
+describe('Type Registration With Subscription Test', function describeCallback() {
+
+  it('should configure registered type', function testCallback() {
+    const key = 'test';
+    const subscriptionKey = 'anyKey';
+    const subscriptionFunction = 'aFunction';
+    container.register(key, TestType)
+      .onNewInstance(subscriptionKey, subscriptionFunction);
+
+    const subscription = container.registrations[key].settings.subscriptions.newInstance[0];
+
+    should(subscription.key).equal(subscriptionKey);
+    should(subscription.method).equal(subscriptionFunction);
+  });
+
+  it('should throw error if no registration is started', function testCallback(next) {
+
+    try {
+      container.onNewInstance('anyKey', 'anyFunction');
+
+    } catch (error) {
+      should(error).not.be.null();
+      next();
+    }
+
+  });
+});
